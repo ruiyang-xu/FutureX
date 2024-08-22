60af94e6ca4958a582600a9ed0f5afbfd73b66a5
wbwang.kaikai <wbwang.kaikai@chinaredstar.com>
2024-08-22 18:37:42 +0800

    05

--- src/pages/portfolio/data.ts
+++ src/pages/portfolio/data.ts
@@ -1,3 +1,22 @@
+import logo01 from "@/assets/img/portfolio/logo_01.png";
+import logo02 from "@/assets/img/portfolio/logo_02.png";
+import logo03 from "@/assets/img/portfolio/logo_03.png";
+import logo04 from "@/assets/img/portfolio/logo_04.png";
+import logo05 from "@/assets/img/portfolio/logo_05.png";
+import logo06 from "@/assets/img/portfolio/logo_06.png";
+import logo07 from "@/assets/img/portfolio/logo_07.png";
+import logo08 from "@/assets/img/portfolio/logo_08.png";
+import logo09 from "@/assets/img/portfolio/logo_09.png";
+import logo10 from "@/assets/img/portfolio/logo_10.png";
+import logo11 from "@/assets/img/portfolio/logo_11.png";
+import logo12 from "@/assets/img/portfolio/logo_12.png";
+import logo13 from "@/assets/img/portfolio/logo_13.png";
+import logo14 from "@/assets/img/portfolio/logo_14.png";
+import logo15 from "@/assets/img/portfolio/logo_15.png";
+import logo16 from "@/assets/img/portfolio/logo_16.png";
+import logo17 from "@/assets/img/portfolio/logo_17.png";
+import logo18 from "@/assets/img/portfolio/logo_18.png";
+
 export const navList = [
   {
     value: 1,
@@ -10,5 +29,234 @@ export const navList = [
   {
     value: 3,
     name: "Project Fund",
+  },
+];
+
+export const fundData = [
+  {
+    logo: logo01,
+    partner: "Series X",
+    year: 2017,
+    type: 1,
+    blurb:
+      "The Attention Factory: Leading a Content Revolution with Powerful Algorithms",
+    dataDetail: {
+      founder: "Yiming Zhang",
+      milestone: "Founded 2012 | 1.9 billion MAU across 150 countries by 2021",
+      website: "bytedance.com/en/",
+    },
+  },
+  {
+    logo: logo02,
+    partner: "Series X",
+    year: 2018,
+    type: 1,
+    blurb: "Made-for-Enthusiasts” Quality Tech, Accessible to Everyone",
+    dataDetail: {
+      founder: "Lei Jun",
+      milestone:
+        "Founded 2010 | IPO 2018 (OTCMKTS: XIACY) | Fortune Global 500 by 2019",
+      website: "mi.com",
+    },
+  },
+  {
+    logo: logo03,
+    partner: "Series D",
+    year: 2018,
+    type: 1,
+    blurb: "Secure and Intelligent Cloud Solutions",
+    dataDetail: {
+      founder: "Lei Jun",
+      milestone: "Founded 2012 | IPO 2020 (NASDAQ: KC) & 2022 (3896.HK)",
+      website: "en.ksyun.com",
+    },
+  },
+  {
+    logo: logo04,
+    partner: "Series C",
+    year: 2014,
+    type: 1,
+    blurb:
+      "One of the Largest Lifestyle Platforms: Everyday Services via Super-App",
+    dataDetail: {
+      founder: "Xing Wang",
+      milestone:
+        "IPO in Hong Kong, 2018; currently the largest food delivery app globally by revenue and usage",
+      website: "Meituan.com",
+    },
+  },
+  {
+    logo: logo05,
+    partner: "Series D",
+    year: 2020,
+    type: 2,
+    blurb: "Most Advanced Distributed SQL Database for Modern Applications",
+    dataDetail: {
+      founder: "Ed Huang, Max Liu, Dylan Cui",
+      milestone: "Founded 2015 | TiDB adopted by 3,000+ companies by 2022",
+      website: "pingcap.com",
+    },
+  },
+  {
+    logo: logo06,
+    partner: "Series E",
+    year: 2021,
+    type: 2,
+    blurb: "AI-Driven IT Operations Management and Monitoring",
+    dataDetail: {
+      founder: "Andy (Jin) Yin",
+      milestone: "Founded 2018 | 1,000+ clients by 2023",
+      website: "cloudwise.ai",
+    },
+  },
+  {
+    logo: logo07,
+    partner: "Series A",
+    year: 2023,
+    type: 2,
+    blurb: "Empowering Creativity Worldwide with Simple and Powerful AI Tools",
+    dataDetail: {
+      founder: "Xinhong Wu",
+      milestone: "Founded 2019 | 42M+ MAU and 2M+ paying customers by 2024",
+      website: "Pixocial.com",
+    },
+  },
+  {
+    logo: logo08,
+    partner: "Series X",
+    year: "20XX",
+    type: 2,
+    blurb: "AI-Powered Personal Life Assistant",
+    dataDetail: {
+      founder: "Hua Mao",
+      milestone: "Founded 2023",
+      website: "Platox.ai",
+    },
+  },
+  {
+    logo: logo09,
+    partner: "Series A",
+    year: 2024,
+    type: 2,
+    blurb:
+      "Asia's Largest Overseas Video Analytics Platform for Content E-commerce",
+    dataDetail: {
+      founder: "Eric Shi",
+      milestone: "Founded 2022",
+      website: "fastmoss.com",
+    },
+  },
+  {
+    logo: logo10,
+    partner: "Series X",
+    year: "20xx",
+    type: 2,
+    blurb:
+      "Disruptive In-Memory Computing (IMC) Technology for Edge Applications",
+    dataDetail: {
+      founder: "Dr. Glenn Ge, Dr. Miao Hu",
+      milestone: "XXX",
+      website: "tetramem.com",
+    },
+  },
+  {
+    logo: logo11,
+    partner: "Series B1",
+    year: 2019,
+    type: 2,
+    blurb: "AI Chipmaker for Autonomous Driving Systems",
+    dataDetail: {
+      founder: "Johnson Shan, Wilson Liu",
+      milestone: "Founded 2016 | IPO 2024 (HKG: 2533)",
+      website: "bst.ai",
+    },
+  },
+  {
+    logo: logo12,
+    partner: "Series A",
+    year: 2021,
+    type: 2,
+    blurb: "AI-First, Comprehensive Autonomous Driving Solutions",
+    dataDetail: {
+      founder: "Xu Lei, Justin Song",
+      milestone: "Founded 2016 | $110M Series B in 2023",
+      website: "nullmax.ai",
+    },
+  },
+  {
+    logo: logo13,
+    partner: "Series A",
+    year: 2020,
+    type: 2,
+    blurb: "Battery-As-A-Service for the Growing EV Network",
+    dataDetail: {
+      founder: false,
+      milestone:
+        "Founded 2020 | 10+ GWh of battery assets under management by 2024",
+      website: "wnpower.cn",
+    },
+  },
+  {
+    logo: logo14,
+    partner: "Series X",
+    year: 2022,
+    type: 2,
+    blurb: "Iconic Sports Car Spirit, Electrified for the Future",
+    dataDetail: {
+      founder: "Qingfeng Feng",
+      milestone: "Founded 2016 | IPO 2024 (Nasdaq: LOT)",
+      website: "lotuscars.com",
+    },
+  },
+  {
+    logo: logo15,
+    partner: "Series A",
+    year: 2021,
+    type: 2,
+    blurb: "AI-Powered Biotech Innovator for Efficient Drug Development",
+    dataDetail: {
+      founder: "Xu Yingqi",
+      milestone: "Founded 2019 | Launched multiple AI platforms by 2024",
+      website: "greatbay-bio.com",
+    },
+  },
+  {
+    logo: logo16,
+    partner: "Series B+",
+    year: 2021,
+    type: 2,
+    blurb: "Leading Online Mental Health and Counseling Platform in China",
+    dataDetail: {
+      founder: "Xu Yingqi",
+      milestone:
+        "Founded 2015 | 40,000+ certified counselors and 30+ million users by 2024",
+      website: "ydl.com",
+    },
+  },
+  {
+    logo: logo17,
+    partner: "Series D",
+    year: 2017,
+    type: 3,
+    blurb: "Revolutionizing Mobility with Intelligent Electric Vehicles",
+    dataDetail: {
+      founder: "William Li",
+      milestone: "Founded 2014 | IPO 2018 (NYSE: NIO)",
+      website: "nio.com",
+    },
+  },
+  {
+    logo: logo18,
+    partner: "Series B",
+    year: 2024,
+    type: 3,
+    blurb:
+      "Open-Source, Lightweight, and Frontier LLMs for Devs and Enterprises",
+    dataDetail: {
+      founder: "Arthur Mensch. Guillaume Lample, Timothée Lacroix",
+      milestone:
+        "Founded May 2023 | Mistral 7B in Sep 2023 | Mixtral 8x7B in Dec 2023 | Mistral Large in Feb 2024",
+      website: "mistral.ai",
+    },
   },
 ];
