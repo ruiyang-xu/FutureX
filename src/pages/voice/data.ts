3726f02f912e4068d44bd6a382cd49781caaf9db
wbwang.kaikai <wbwang.kaikai@chinaredstar.com>
2024-08-27 09:10:42 +0800

    13

--- src/pages/voice/data.ts
+++ src/pages/voice/data.ts
@@ -4,6 +4,18 @@ export const navList: VoiceNav[] = [
     name: "all",
   },
   {
+    value: "event",
+    name: "event",
+  },
+  {
+    value: "insight",
+    name: "insight",
+  },
+  {
+    value: "spotlight",
+    name: "spotlight",
+  },
+  {
     value: "seminar",
     name: "seminar",
   },
@@ -24,7 +36,7 @@ export const navList: VoiceNav[] = [
 export const newList: NewList[] = [
   {
     id: 1,
-    type: "news",
+    type: "event",
     title: "FutureX's Silicon Valley AI Tour: A Recap",
     desc: `FutureX Capital recently wrapped up a successful AI-focused trip to Silicon Valley. Themed ‘AI Applications,’ the event delved into integrating AI with fields like AI agents, content commerce, hardware, and social media. Our mission is to discover and foster continuous innovation and breakthroughs in these areas.
 
@@ -36,7 +48,7 @@ This was FutureX’s third event in Silicon Valley, the epicenter of innovation.
   },
   {
     id: 2,
-    type: "news",
+    type: "insight",
     title:
       "How AI + Emotional Companionship Is Shaping a Billion-Dollar Future | FutureX Seminar",
     desc: `As technology continues to evolve, a new frontier is emerging at the intersection of LLM and emotional companionship. During a recent seminar hosted by FutureX, industry experts delved into how large language models (LLMs) are not just enhancing technological capabilities but are also poised to unlock vast investment opportunities in a sector that could soon be worth billions. This seminar, driven by a year of comprehensive research, showcased the transformative potential of AI in meeting deep human emotional needs, predicting significant social and business impacts. Below are the key insights from this illuminating session."`,
@@ -45,7 +57,7 @@ This was FutureX’s third event in Silicon Valley, the epicenter of innovation.
   },
   {
     id: 3,
-    type: "news",
+    type: "insight",
     title: "FutureX Perspective: Insights from NVIDIA GTC 2024",
     desc: `Last month, the NVIDIA 2024 Global Technology Conference (GTC) captivated the global AI community, filling every seat. The FutureX team had the privilege of joining tech leaders from around the world at this pivotal event, which highlighted NVIDIA's technological milestones and the future trajectory of AI.`,
     date: "April 15, 2024",
@@ -53,7 +65,7 @@ This was FutureX’s third event in Silicon Valley, the epicenter of innovation.
   },
   {
     id: 4,
-    type: "news",
+    type: "insight",
     title:
       "A Deep Dive into 2024's AI Investment Trends: Insights from Silicon Valley",
     desc: `Recently, FutureX hosted a seminar focused on exploring the investment trends and future directions of the AI industry. This event attracted a broad spectrum of attendees within the FutureX ecosystem. Distinguished guests, including the chairman of Open Source China and the well-known Twitter AI blogger @FinanceYF5, Will, alongside other industry experts, shared their insights on AI development trends and investment opportunities. Below is a summary of the key highlights:`,
@@ -62,7 +74,7 @@ This was FutureX’s third event in Silicon Valley, the epicenter of innovation.
   },
   {
     id: 5,
-    type: "news",
+    type: "spotlight",
     title:
       "FutureX Capital Celebrates Lotus Technology's Historic NASDAQ Debut",
     desc: `FutureX Capital is proud to announce that its portfolio company, Lotus Technology, has made history with its recent merger with SPAC company L Catterton Asia Acquisition Corp (LCAA). As of February 23rd, 2024, Lotus Technology has officially gone public on NASDAQ under the ticker symbol "LOT". This significant milestone represents the fastest Initial Public Offering (IPO) in the history of the electric vehicle sector, and the largest IPO from Chinese companies in the last two years. FutureX Capital, which participated in the Pre-A round financing at the inception of Lotus Technology in 2021, lauds this achievement as a testament to the company's exceptional growth and the rapidly expanding global electric vehicle market.
@@ -72,7 +84,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 6,
-    type: "news",
+    type: "spotlight",
     title:
       "The Evolution and Constants of FutureX: Persisting in the Hard but Right Choices",
     desc: `In the venture capital and business circles this year, there’s a widespread sense of resignation and anxiety. However, looking back at past challenges such as 9/11, the 2008 economic crisis, and the SARS epidemic, it's evident that as long as we continue to grow, we can overcome any difficulty. Despite a current domestic atmosphere tinged with pessimism, FutureX has achieved rapid development over the past six years, maintaining a positive attitude, struggling alongside the entire FutureX ecosystem, and exploring the boundless possibilities of future technology.`,
@@ -81,7 +93,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 7,
-    type: "news",
+    type: "insight",
     title: "AI Investment Guide: Unearthing Trillion-Dollar Market Potential",
     desc: `According to a McKinsey report, the new generation of artificial intelligence technology is expected to create a massive market opportunity worth $2.6 to $4.4 trillion across various industries. While the specific figures are not the central focus, the scale they represent is significant. The widespread application of AI heralds the emergence of a new round of industry giants. Based on this, we have conducted an in-depth analysis of the AI application market and discussed the investment opportunities we have observed.`,
     date: "January 8, 2024",
@@ -89,7 +101,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 8,
-    type: "news",
+    type: "event",
     title:
       "Connecting the Intelligence, Explore the Infinite Possibilities of AI",
     desc: `Recently, FutureX Capital convened the 2023 Investors Summit in Zhuhai. The annual meeting, under the theme FutureX Intelligence Connection, Exploring the Infinite Possibilities of AI,' invited more than 50 distinguished figures from the industry. This included frontline technology entrepreneurs and top experts in areas such as semiconductors, cloud computing, and artificial intelligence. They joined investors in discussing the trends in technological development and future investment opportunities. `,
@@ -98,7 +110,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 9,
-    type: "news",
+    type: "other",
     title:
       "FutureX Fund Management (Hong Kong) Limited - Climate-Related Risks Disclosure 2023",
     desc: `FutureX Fund Management (Hong Kong) Limited acknowledges that climate change is emerging as a major driving factor affecting long term resilience in the industry and the global economy. Thereby, this creates an urgent need to accelerate the transition towards a net-zero economy. While climate change poses a systemic and unprecedented risk to the global economy, the impacts on specific markets, regions, communities, and investments are complex, dynamic, and uncertain.`,
@@ -107,7 +119,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 10,
-    type: "news",
+    type: "spotlight",
     title: "Congratulations to Cryofocus Medtech for its successful IPO!",
     desc: `On December 30, 2022, FutureX Capital’s portfolio company Cryofocus Medtech (Shanghai) Co., Ltd. (“Cryofocus”) was listed on the Main Board of The Stock Exchange of Hong Kong Limited (HKSE: 6922). As Cryofocus Medtech’s Series B investor, we are optimistic about the leadership position of the company in the cryoablation industry and mass adoption of its technology in the future.`,
     date: "December 30, 2022",
@@ -115,7 +127,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 11,
-    type: "news",
+    type: "spotlight",
     title:
       "World-changing Disruptive Innovation Company: Navigating the Opportunites and Challenges featuring ICLeague Foudner Dr Simon Yang",
     desc: `At FutureX Capital, we invest in next-generation leaders who are building the future through technology innovation. As always, we are commited to bringing our tech founders and experts together and having a conversation with our community. On 26th November, 2022, we organized the “World-changing Disruptive Innovation Company: Navigating the Opportunites and Challenges” in-person seminar (with livestreaming session) in Singapore - where we were honored to have Dr. Shining (Simon) Yang , the founder of ICLeague, with us and to share his insights on disruptive innovation in semiconductor industry. Below are the key takeaways from Dr. Yang’s sharing.`,
@@ -124,7 +136,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 12,
-    type: "news",
+    type: "other",
     title:
       "FutureX Fund Management (Hong Kong) Limited - Climate-Related Risks Disclosure",
     desc: `FutureX Fund Management (Hong Kong) Limited acknowledges that climate change is emerging as a major driving factor affecting long term resilience in the industry and the global economy. Thereby, this creates an urgent need to accelerate the transition towards a net-zero economy. While climate change poses a systemic and unprecedented risk to the global economy, the impacts on specific markets, regions, communities, and investments are complex, dynamic, and uncertain.
@@ -134,7 +146,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 13,
-    type: "news",
+    type: "event",
     title: `FutureX Capital participated in the "Share and Love" charity event by Shagnhai ADream Foundation`,
     desc: `FutureX Fund Management (Hong Kong) Limited acknowledges that climate change is emerging as a major driving factor affecting long term resilience in the industry and the global economy. Thereby, this creates an urgent need to accelerate the transition towards a net-zero economy. While climate change poses a systemic and unprecedented risk to the global economy, the impacts on specific markets, regions, communities, and investments are complex, dynamic, and uncertain.
 `,
@@ -143,7 +155,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 14,
-    type: "news",
+    type: "spotlight",
     title: `WeLion New Energy closes RMB 1.5Bn Series D funding round | FutureX Portfolio`,
     desc: `WeLion New Energy Technology Co., Ltd., ("WeLion New Energy"), a portfolio company of FutureX and an industry leader in solid-state lithium batteries, closed a RMB 1.5 billion Series D round. Investors were led by China Chengtong Mixed Reform Fund, followed by CITIC Construction Investment, SDIC ChuangYi Industry Fund, China Merchants Capital, Dyee Capital, Hidden Hill Capital, Hermitage Capital, Huzhou Economic Development, Yunhe Partners, and other investors. The round was followed by Zibo Jingneng, which also participated in earlier fundraising rounds. Lighthouse Capital acted as the exclusive financial advisor for this round of financing. After the financing is completed, the company will further speed up its production line expansion, as well as its investments towards R&D.
 `,
@@ -152,7 +164,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 15,
-    type: "news",
+    type: "event",
     title: `Cynthia Zhang, Founding Partner of FutureX Capital, Invited to 2022 Hong Kong Fintech Week to Talk about Financing the Companies of Future
 `,
     desc: `On 31 October 2022, FutureX Capital’s Founding Partner, Ms Cynthia Zhang, was invited as guest speaker of the 2022 Hong Kong Fintech Week's Financing the Companies of Tomorrow Panel.
@@ -162,7 +174,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 16,
-    type: "news",
+    type: "insight",
     title: `New Energy Sector Changes and Investment Opportunities in the Context of Carbon Neutrality – Part I｜FutureX Webinar`,
     desc: `On July 12, 2022, FutureX held an online seminar on the topic of " Changes and Opportunities in the New Energy Sector in the Era of Carbon Neutrality", and invited world-class experts to share and discuss the topic.
 `,
@@ -171,7 +183,7 @@ The global investors warmly welcomed Lotus Technology's debut on the NASDAQ mark
   },
   {
     id: 17,
-    type: "news",
+    type: "spotlight",
     title: `FutureX-backed Jenscare Scientific Lists on the Hong Kong Stock Exchange`,
     desc: `  On October 10, 2022, FutureX Capital’s portfolio company Ningbo Jenscare Scientific Co. Ltd. (stock code: 9877.HK), a medical device company focusing on developing innovative products for the treatment of structural heart disease, was successfully listed on the Main Board of Hong Kong Exchanges and Clearing (HKEX).
 `,
